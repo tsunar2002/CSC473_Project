46ff11fd48331913f592c6c261dda29f829788ae
jkim4264@gmail.com <jkim4264@gmail.com>
2024-11-15 16:48:18 -0500

    Edited trail card components

--- csc473_project/app/page.tsx
+++ csc473_project/app/page.tsx
@@ -4,14 +4,16 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center text-center">
       <h1 className="text-4xl font-bold mt-10 mb-4">
-        This is the landing page
+        TrailTales
       </h1>
       <div className="flex gap-4 justify-center">
         <TrailsCard />
         <TrailsCard />
         <TrailsCard />
+        <TrailsCard />
       </div>
       <div className="flex gap-4 justify-center">
+        <TrailsCard />
         <TrailsCard />
         <TrailsCard />
         <TrailsCard />
