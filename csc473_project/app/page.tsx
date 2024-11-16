47ba2f96e6fa2d14b12060a45fbb46023e4fa6e8
jkim4264@gmail.com <jkim4264@gmail.com>
2024-11-16 17:45:03 -0500

    Added Header with Search Bar

--- csc473_project/app/page.tsx
+++ csc473_project/app/page.tsx
@@ -1,23 +1,49 @@
 import { TrailsCard } from "@/components/TrailsCard/TrailsCard";
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
+import trailLogo from "../public/assets/TrailLogo.png"
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center text-center">
-      <h1 className="text-4xl font-bold mt-10 mb-4">
-        TrailTales
-      </h1>
-      <div className="flex gap-4 justify-center">
-        <TrailsCard />
-        <TrailsCard />
-        <TrailsCard />
-        <TrailsCard />
+    <>
+      <div className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+        <a className="flex lg:flex-1 items-center gap-3">
+          <Image
+            src= {trailLogo}
+            height="60"
+            width="60"
+            alt=""
+          />
+          <span className="text-lg font-bold">TrailTales</span>
+        </a>
+
+        <div className="lg:flex justify-center">
+          <Input 
+            id="trailSearch" 
+            placeholder="Search For Trails" 
+            type="text"
+            className="" />
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a className="text-sm/6 font-semibold text-gray-900">Log in</a>
+        </div>
       </div>
-      <div className="flex gap-4 justify-center">
-        <TrailsCard />
-        <TrailsCard />
-        <TrailsCard />
-        <TrailsCard />
+
+      <div className="flex flex-col items-center text-center">
+        <div className="flex gap-4 justify-center">
+          <TrailsCard />
+          <TrailsCard />
+          <TrailsCard />
+          <TrailsCard />
+        </div>
+        <div className="flex gap-4 justify-center">
+          <TrailsCard />
+          <TrailsCard />
+          <TrailsCard />
+          <TrailsCard />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- csc473_project/components/TrailsCard/TrailsCard.tsx
+++ csc473_project/components/TrailsCard/TrailsCard.tsx
@@ -12,7 +12,7 @@ export function TrailsCard() {
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[20rem] h-auto rounded-xl p-6 border  ">
         <CardItem translateZ="100" className="w-full mt-4">
           <Image
-            src={leavesImage}
+            src="https://images.unsplash.com/photo-1507041957456-9c397ce39c97?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             height="1000"
             width="1000"
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
