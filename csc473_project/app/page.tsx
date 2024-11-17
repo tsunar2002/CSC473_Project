4fc89622e3648a8dd3ef446034f9b3022b170939
tsunar2002 <118147365+tsunar2002@users.noreply.github.com>
2024-11-17 14:37:49 -0500

    wrote a controller function to fetch Trails Data and display it on TrailsCard

--- csc473_project/app/page.tsx
+++ csc473_project/app/page.tsx
@@ -1,29 +1,80 @@
+"use client";
 import { TrailsCard } from "@/components/TrailsCard/TrailsCard";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
-import trailLogo from "../public/assets/TrailLogo.png"
+import trailLogo from "../public/assets/TrailLogo.png";
+import { useEffect, useState } from "react";
+import fetchTrails from "@/controllers/trailsData";
 
+interface Trail {
+  id: string;
+  name: string;
+  difficulty: string;
+  city: string;
+  rating: string;
+  thumbURL: string;
+}
 
 export default function Home() {
+  const [city, setCity] = useState("");
+  const [trails, setTrails] = useState<Trail[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function getTrailsData() {
+      const trailsData = await fetchTrails("Albuquerque");
+      setTrails(trailsData.trails.slice(0, 4));
+    }
+
+    getTrailsData();
+  }, []);
+
+  // Function to get the city from input
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setCity(event.target.value);
+  }
+
+  // Function to handle Search
+  async function handleSearchButton() {
+    if (!city) {
+      setError("Please type in a city name!");
+      return;
+    }
+
+    const data = await fetchTrails(city);
+    if (!data) {
+      setError(
+        "We do not have info about hiking spots in that location in our database!"
+      );
+      return;
+    }
+    // For now only getting 4 results beacuse of UI problems.
+    setTrails(data.trails.slice(0, 4));
+    setError("");
+  }
+
   return (
     <>
       <div className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <a className="flex lg:flex-1 items-center gap-3">
-          <Image
-            src= {trailLogo}
-            height="60"
-            width="60"
-            alt=""
-          />
+          <Image src={trailLogo} height="60" width="60" alt="" />
           <span className="text-lg font-bold">TrailTales</span>
         </a>
 
         <div className="lg:flex justify-center">
-          <Input 
-            id="trailSearch" 
-            placeholder="Search For Trails" 
+          <Input
+            id="trailSearch"
+            placeholder="Search For Trails"
             type="text"
-            className="" />
+            className=""
+            onChange={handleInputChange}
+          />
+          <button
+            className="ml-4 px-6 py-2 bg-blue-600 text-white font-semibold rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all"
+            onClick={handleSearchButton}
+          >
+            SEARCH
+          </button>
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a className="text-sm/6 font-semibold text-gray-900">Log in</a>
@@ -31,17 +82,17 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col items-center text-center">
-        <div className="flex gap-4 justify-center">
-          <TrailsCard />
-          <TrailsCard />
-          <TrailsCard />
-          <TrailsCard />
-        </div>
-        <div className="flex gap-4 justify-center">
-          <TrailsCard />
-          <TrailsCard />
-          <TrailsCard />
-          <TrailsCard />
+        <div className="flex gap-10 justify-center">
+          {trails.map((trail) => (
+            <TrailsCard
+              key={trail.id}
+              trailName={trail.name}
+              difficultyScore={trail.difficulty}
+              cityName={trail.city}
+              rating={trail.rating}
+              imageURL={trail.thumbURL}
+            />
+          ))}
         </div>
       </div>
     </>
--- csc473_project/components/TrailsCard/TrailsCard.tsx
+++ csc473_project/components/TrailsCard/TrailsCard.tsx
@@ -6,13 +6,27 @@ import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
 import Link from "next/link";
 import leavesImage from "../../public/assets/leaves.jpg";
 
-export function TrailsCard() {
+interface TrailsCardProps {
+  trailName?: string;
+  difficultyScore?: string;
+  cityName?: string;
+  rating?: string;
+  imageURL?: string;
+}
+
+export function TrailsCard({
+  trailName = "TrailName",
+  difficultyScore = "Not enough info",
+  cityName = "CityName",
+  rating = "rating",
+  imageURL = "https://images.unsplash.com/photo-1507041957456-9c397ce39c97?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+}: TrailsCardProps) {
   return (
     <CardContainer className="inter-var">
       <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[20rem] h-auto rounded-xl p-6 border  ">
         <CardItem translateZ="100" className="w-full mt-4">
           <Image
-            src="https://images.unsplash.com/photo-1507041957456-9c397ce39c97?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src={imageURL}
             height="1000"
             width="1000"
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
@@ -23,28 +37,33 @@ export function TrailsCard() {
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
-          Trail Name
+          {trailName}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
-          Difficulty: Medium
+          {cityName}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
-          Distance: 1.9 mi.
+          Difficulty:{" "}
+          {parseInt(difficultyScore) < 3
+            ? "Easy"
+            : parseInt(difficultyScore) > 3
+            ? "Hard"
+            : "Medium"}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
-          Time Estimated: 1 hr. 15 min.
+          Rating: {rating}
         </CardItem>
 
         <div className="flex justify-between items-center mt-10">
@@ -56,7 +75,6 @@ export function TrailsCard() {
             Explore
           </CardItem>
         </div>
-
       </CardBody>
     </CardContainer>
   );
