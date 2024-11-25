bdcbc63ac8ec3213e06a87fa17ff85467ea5878d
tsunar2002 <118147365+tsunar2002@users.noreply.github.com>
2024-11-24 19:36:55 -0500

    Clerk Authentication and protected some routes

---
+++ csc473_project/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,24 @@
+import { SignIn } from "@clerk/nextjs";
+import leavesImage from "../../../public/assets/leaves.jpg";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div className="flex h-screen">
+      {/* Left Image Side */}
+      <div className="w-1/2 overflow-hidden">
+        <Image
+          src={leavesImage}
+          alt="Shopping bag"
+          className="w-full h-full object-cover"
+        />
+      </div>
+
+      {/* Right Form Side */}
+      <div className="w-1/2 flex flex-col justify-center items-center p-8">
+        <SignIn />
+      </div>
+    </div>
+  );
+}
