ee63fe50b4a149d84b97756f2e575eace89af586
jkim3273 <jkim4264@gmail.com>
2024-12-14 20:02:13 -0500

    Sample e2e/api testing

--- csc473_project/cypress/e2e/home_page_spec.cy.ts
+++ csc473_project/cypress/e2e/home_page_spec.cy.ts
@@ -1,13 +1,23 @@
+import { setupClerkTestingToken } from '@clerk/testing/cypress'
+
+describe('Signup Test', () => {
+  it('sign up', () => {
+    setupClerkTestingToken()
+
+    cy.visit('/sign-up')
+    // Add any other actions to test
+  })
+})
+
 describe('Home Page Launch', () => {
   it('Should be able to load home page', () => {
-    cy.visit("http://localhost:3000/")
+    cy.visit("/")
   })
 })
 
 describe('Home Page API Loading', () => {
   it('Should load trailCards API', () => {
-    cy.visit("http://localhost:3000/")
+    cy.visit("/")
     cy.contains("Angels Landing Trail")   
   })
 })
-
