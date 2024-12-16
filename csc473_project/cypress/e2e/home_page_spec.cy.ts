dcdbfef197ffcfcc333ed57b79b615ca49149760
jkim3273 <jkim4264@gmail.com>
2024-12-16 01:14:49 -0500

    Added tests for querying and rerouting

--- csc473_project/cypress/e2e/home_page_spec.cy.ts
+++ csc473_project/cypress/e2e/home_page_spec.cy.ts
@@ -9,6 +9,25 @@ describe('Home Page Launch', () => {
 describe('Home Page API Loading', () => {
   it('Should load trailCards API', () => {
     cy.visit("/")
+    cy.contains("John Muir Trail")  
     cy.contains("Angels Landing Trail")   
+  })
+})
+
+describe('Search Bar Visibility', () => {
+  it('Search Bar should be visible', () => {
+    cy.visit("/")
+    cy.get('input').should('be.visible');
+  })
+})
+
+describe('Search Bar Test Query for Oregon', () => {
+  it('Should show trails for Oregon', () => {
+    cy.visit("/")
+    cy.get('input').type("Oregon{enter}")
+    cy.get('button')
+    cy.contains("Cascade Pass Trail")
+    cy.contains("Mount Hood Timberline Trail")
+    cy.should('not.contain', "Angels Landing Trail")
   })
 })
--- csc473_project/cypress/e2e/misc_pages_spec.cy.ts
+++ csc473_project/cypress/e2e/misc_pages_spec.cy.ts
@@ -1,16 +1,31 @@
 import { setupClerkTestingToken } from '@clerk/testing/cypress'
 
+describe('About Page Button Visibility', () => {
+    it('Should show button on about page', () => {
+        cy.visit("/about")
+        cy.get('p').contains('Start Exploring Trails')
+    })
+})
+
+describe('About Page Button Reroute', () => {
+    it('Should be able to load home page from about', () => {
+        cy.visit("/about")
+        cy.get('p').contains('Start Exploring Trails').click()
+        cy.url().should('eq', 'http://localhost:3000/')
+    })
+})
+
+describe('Sign In Reroute', () => {
+    it('Should reroute to authentication when visiting feed page', () => {
+        cy.visit("/feed")
+        cy.url().should('contain', '/sign-in')
+    })
+})
+
 describe('Signup Test', () => {
     it('sign in', () => {
         cy.visit(`/`)
         cy.clerkSignIn({ strategy: 'email_code', identifier: 'testuser+clerk_test@example.com' })
         cy.visit('/favorites')
-        // user is signed in from here on
-    })
-})
-
-describe('About Page Launch', () => {
-    it('Should be able to load about page', () => {
-        cy.visit("/about")
     })
 })
